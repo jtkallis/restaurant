8de61d41dc30576279df91ebbf3a6c5109aca7a9
John <jtkallis@gmail.com>
2023-04-02 04:12:10 -0400

    edited home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-list>
-      <h1 id="restaurant-name">Restaurant Home Page</h1>
-  </v-list>
+  <v-card>
+      <v-card-title id="restaurant-name">Restaurant Home Page</v-card-title>
+      <v-card-item>
+        /Menu page displays all of the menus
+      </v-card-item>
+      <v-card-item>
+        /Order displays only the menu available at a given time
+      </v-card-item>
+      <v-card-item>
+        /editMenu allows for edits to the menu
+      </v-card-item>
+      <v-card-ite>
+        Menu Items are of this structure
+        {
+          name: "Steak",
+          price: 0,
+          description: "",
+          section_id: "",
+          options: ["temperatures","sides"]
+        }
+        where sides contains an array of items that also have an options array value.
+        the second modal is nested in case sub items have options
+      </v-card-ite>
+  </v-card>
 </template>
