8281d8cae90b02f2b713987492d67d3bc0da5925
John <jtkallis@gmail.com>
2023-04-02 01:22:58 -0400

    removed styling

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,3 @@
       <h1 id="restaurant-name">Restaurant Home Page</h1>
   </v-list>
 </template>
-<style>
-#restaurant-name {
-  font-family:'Times New Roman', Times, serif;
-  -webkit-text-stroke: 3px darkgreen;
-  color: white;
-}
-</style>
